1e840e316d9caee445e8624267a71473bc32d936
Johannes Borgström <johannes@highspirits.se>
2017-11-30 00:36:45 +0100

    Isometric conversion added

--- src/components/Monitor/Monitor.js
+++ src/components/Monitor/Monitor.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react'
-import { TILE_WIDTH, TILE_HEIGHT } from '../../constants'
+import { TILE_WIDTH, TILE_HEIGHT, PERSP_ANGLE, OPP_ANGLE } from '../../constants'
 import utils from '../../utils'
 import './Monitor.css'
 
 const Point = (cmd, x, y, z) => {
   return { cmd, x, y, z }
 }
-
+const toRadians = (angle) => {
+  return angle * (Math.PI / 180)
+}
 const dFrom3DPoints = (points, offset) => {
-  console.log(offset)
-  let unit = TILE_WIDTH / 2
+  let unit = TILE_WIDTH
+  let perspAngleSin = Math.sin(toRadians(PERSP_ANGLE))
+  let oppAngleSin = Math.sin(toRadians(OPP_ANGLE))
   let d = points.map((point, i) => {
-    let x = point.x * unit + (point.z * unit / 2)
-    let y = unit * point.y - (point.z * unit / 2)
+    let x = point.x * unit * oppAngleSin + (point.z * unit) * oppAngleSin
+    let y = (point.y * oppAngleSin) * unit + point.x * unit * perspAngleSin - point.z * unit * perspAngleSin
     return `${point.cmd}${offset.x + x} ${offset.y + y}`
   })
   d.push('Z')
@@ -33,14 +36,14 @@ const Monitor = ({ gridTile }) => {
     new Point('L', 1, 0, 0),
     new Point('L', 1, 0, 1),
     new Point('L', 0, 0, 1),
-  ], {x: coords.x, y: coords.y})
-  
+  ], { x: coords.x, y: coords.y })
+
   let topSquare = dFrom3DPoints([
     new Point('M', 0, 1, 0),
     new Point('L', 1, 1, 0),
     new Point('L', 1, 1, 1),
     new Point('L', 0, 1, 1),
-  ], {x: coords.x, y: coords.y})
+  ], { x: coords.x, y: coords.y })
 
   return (
     <g className='monitor'>
--- src/constants.js
+++ src/constants.js
@@ -4,3 +4,5 @@ export const TILES_IN_X = 10
 export const TILES_IN_Y = 6
 export const GRID_WIDTH = TILES_IN_X * TILE_WIDTH
 export const GRID_HEIGHT = TILES_IN_Y * TILE_HEIGHT
+export const PERSP_ANGLE = 26.565
+export const OPP_ANGLE = 90 - PERSP_ANGLE
